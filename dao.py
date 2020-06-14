2371f571f96d318fc37726f9a5b182d7869f3e72
hminkoo10 <62460314+hminkoo10@users.noreply.github.com>
2020-06-14 11:29:00 +0900

    asd
    asd

--- dao.py
+++ dao.py
@@ -22,7 +22,7 @@ tkdyd = []
 check = []
 bot = commands.Bot(command_prefix=',')
 PRM = ['657773087571574784','564250827959566359','694406375228440606']
-token = "NzEzMDA3Mjk2NDc2NzQxNjQz.Xs5beA.pDkDd1FmNVNjlEc7eAyO8HJS3IQ"
+token = "NzEzMDA3Mjk2NDc2NzQxNjQz.XuWK4w.1D-nap9ca7zYP__JuEwdxiQ4ZEU"
 DBKR_token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjcxMzAwNzI5NjQ3Njc0MTY0MyIsImlhdCI6MTU5MTEwNDk5MywiZXhwIjoxNjIyNjYyNTkzfQ.DusY04FtN-Gry0H9WP-pnLFqWkTg1TuKAyM9fzklDJedqjKk4VIpgk6SC70p1xZfQ_e08kOE_sGS-Vd5alI0U3JO3a_l2VIGZFAno2f79jU4ZRTbLKKKCEhY8eLGQ__rAawAbV8vgXrS0HWtM3fQEE23ud7DriLJAuRjn9Cgvjg"
 progress = "none"
 player1 = "none"
