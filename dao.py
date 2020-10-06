ed2633c7fb68f957d0f77e18a07dfb8db7425bff
hminkoo10 <62460314+hminkoo10@users.noreply.github.com>
2020-10-06 22:03:44 +0900

    Update dao.py

--- dao.py
+++ dao.py
@@ -2471,6 +2471,9 @@ async def 코로나현황(ctx):
         driver = webdriver.Chrome()
     elif platform.system() == "Linux":
         driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver',chrome_options=webdriver.ChromeOptions())
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
     driver.get('https://search.naver.com/search.naver?ie=UTF-8&sm=whl_hty&query=%EC%BD%94%EB%A1%9C%EB%82%98%ED%98%84%ED%99%A9#')
     #driver.get('https://coronaboard.kr')
     driver.execute_script("window.scrollTo(1000, 1150)")
